ad2983078bbcf6ccc99f1a0112662de05acbdaab
Rickey Bowers Jr <bitRAKE@gmail.com>
2020-05-13 22:24:35 -0600

    more problems

--- math/Project Euler/P020.asm
+++ math/Project Euler/P020.asm
@@ -1,48 +1,43 @@
-macro PROBLEM
-	lea ebp, [Number]
-
-
-
-	mov eax,1
-	mov ecx,1
-@@:	lea ecx,[rcx+1]
-	cmp ecx,101
-	jz last
-	imul rax,rcx
-	jnc @B
-
-
-
-
-
+macro PROBLEM ; very close to problem 16.
+	local calc,mlt,grew,_0,_1
 
 	; set up number in binary
-	mov ebx, 100
-@@:	invoke mpn_mul_1, ebp, ebx, 0
-	dec ebx
-	jne @B
 
-
-
-
-
-
-	; find used limbs
-	mov edi, Number.limbs
-@@:	cmp DWORD [rbp+(rdi-1)*4], 0
-	lea edi,[rdi-1]
-	jz @B
+	xor edi,edi		; track growth of big integer
+	mov ebx,1		; product of [1..100]
+calc:	xor esi,esi
+	xor edx,edx
+	pushfq
+mlt:	mov eax,[bignum+rsi*4]
+	mov [bignum+rsi*4],edx
+	mul ebx
+	popfq
+	adc [bignum+rsi*4],eax
+	pushfq
+	add esi,1		; higher limbs of big integer
+	cmp edi,esi		; past the end?
+	jnc mlt
+	add ebx,1		; multiply by larger numbers
+	popfq
+	adc edx,0		; carry remaining means it needs to grow
+	jz grew
+	mov [bignum+rdi*4+4],edx
+	add edi,1
+grew:	cmp ebx,101		; only 3ms for 964!
+	jnz calc
 
 	; convert to decimal to sum digits 
+
+	lea ebp, [bignum]
 	mov ecx, 10
-	xor ebx, ebx ; sum
-_0:	mov esi, edi ; dwords to convert
+	xor ebx, ebx		; sum
+_0:	mov esi, edi		; dwords to convert
 	xor edx, edx
-@@:	mov eax, [rbp+rsi*4]
+_1:	mov eax, [rbp+rsi*4]
 	div ecx
 	mov [rbp+rsi*4], eax
 	dec esi
-	jns @B
+	jns _1
 	add ebx, edx
 	cmp DWORD [rbp+rdi*4], 0
 	jnz _0
@@ -56,10 +51,5 @@ INCLUDE ".\support\win64.inc"
 
 macro __DATA ; must be placed after support include (if present)
 ALIGN 64
-	BIGINT STRUCT
-		limbs	DWORD ?
-	BIGINT ENDS
-	BIGINT <1024>
-	Number DWORD 1, 1023 DUP (0)
+	bignum dd 1, 255 DUP (?) ; good up to 964! (c:
 end macro
-; veery close to problem 16.
---
+++ math/Project Euler/P021.asm
@@ -0,0 +1,47 @@
+macro PROBLEM
+
+LIMIT = 10000
+
+	xor edi,edi
+	mov eax,LIMIT
+SumOfDiv:
+	mov ebx,1		; all numbers divisible by one
+	mov ecx,1
+	push rax
+DNxt:	add ecx,1
+	mov eax,[rsp]
+	xor edx,edx
+	div ecx
+	test edx,edx
+	jnz xdiv
+	add ebx,ecx	; add proper divisor
+	cmp eax,ecx
+	jz square	; done, have sum of divisors
+	add ebx,eax
+xdiv:	cmp eax,ecx	; TODO: remove same CMP instruction above this one
+	jnc DNxt
+
+; have sum of proper divisors of [RSP] in RBX
+
+square:	pop rax
+	mov [nRay+(rax-1)*4],ebx	; store sum of divisors
+	cmp eax,ebx			; could we have found it's pair?
+	jnc INxt
+	cmp ebx,LIMIT			; is it in array?
+	ja INxt
+	cmp [nRay+(rbx-1)*4],eax	; does it match?
+	jnz INxt
+	add edi,ebx
+	add edi,eax
+INxt:	sub eax,1
+	jnz SumOfDiv
+
+	SOLUTION "Problem 21: %llu", rdi
+end macro
+
+INCLUDE ".\support\win64.inc"
+
+macro __DATA ; must be placed after support include (if present)
+ALIGN 64
+	nRay rd LIMIT
+end macro
